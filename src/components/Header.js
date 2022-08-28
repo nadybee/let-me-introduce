06ab77deafe6e86a5bf99dcfbe7ce320744d1738
Nadybee <natalie@yoodlize.com>
2022-08-27 18:19:00 -0600

    header done

---
+++ src/components/Header.js
@@ -0,0 +1,364 @@
+/* This example requires Tailwind CSS v2.0+ */
+
+import { Fragment } from "react"
+import { Popover, Transition } from "@headlessui/react"
+import {
+Squares2X2Icon,
+  Bars3Icon,
+  BookmarkSquareIcon,
+  CalendarIcon,
+  NewspaperIcon,
+  HeartIcon,
+  SunIcon,
+LightBulbIcon,
+  LifebuoyIcon,
+  ShieldCheckIcon,
+  XMarkIcon,
+  CakeIcon,
+} from "@heroicons/react/24/outline"
+import { ChevronDownIcon } from "@heroicons/react/20/solid"
+
+const projects = [
+  {
+    name: "The Wedding Loop",
+    href: "#",
+    description:
+      "Users can manage their guest list and let guests upload pictures to their wedding album",
+    icon: CakeIcon,
+  },
+  {
+    name: "Plan My Date App",
+    href: "#",
+    description:
+      "Users can let the app choose a random resturant in their area, along with a random activity",
+    icon: LightBulbIcon,
+  },
+  {
+    name: "BlackJack",
+    href: "#",
+    description: "Classic BlackJack using OOP Vanilla Javascript",
+    icon: HeartIcon,
+  },
+  {
+    name: "Weather Dashboard",
+    href: "#",
+    description: "App to check the weather",
+    icon: SunIcon,
+  },
+
+  {
+    name: "Mock Tech Blog",
+    href: "#",
+    description:
+      "A mock blog built with the MVC model",
+    icon: NewspaperIcon,
+  },
+]
+const callsToAction = [
+  { name: "See All Projects", href: "#", icon: Squares2X2Icon }
+ 
+]
+const resources = [
+  {
+    name: "Help Center",
+    description:
+      "Get all of your questions answered in our forums or contact support.",
+    href: "#",
+    icon: LifebuoyIcon,
+  },
+  {
+    name: "Guides",
+    description:
+      "Learn how to maximize our platform to get the most out of it.",
+    href: "#",
+    icon: BookmarkSquareIcon,
+  },
+  {
+    name: "Events",
+    description:
+      "See what meet-ups and other events we might be planning near you.",
+    href: "#",
+    icon: CalendarIcon,
+  },
+  {
+    name: "Security",
+    description: "Understand how we take your privacy seriously.",
+    href: "#",
+    icon: ShieldCheckIcon,
+  },
+]
+// const recentPosts = [
+//   { id: 1, name: "Boost your conversion rate", href: "#" },
+//   {
+//     id: 2,
+//     name: "How to use search engine optimization to drive traffic to your site",
+//     href: "#",
+//   },
+//   { id: 3, name: "Improve your customer experience", href: "#" },
+// ]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
+}
+
+export default function Header() {
+  return (
+    <div className="relative bg-gray-50">
+      <Popover className="relative bg-white shadow">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6">
+          <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+            <div className="flex justify-start lg:w-0 lg:flex-1">
+                <h1 className="text-xl md:text-3xl text-red-700"> Natalie Fairbourne Portfolio</h1>
+            </div>
+            <div className="-mr-2 -my-2 md:hidden">
+              <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
+                <span className="sr-only">Open menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </Popover.Button>
+            </div>
+            <Popover.Group as="nav" className="hidden md:flex space-x-10">
+              <Popover className="relative">
+                {({ open }) => (
+                  <>
+                    <Popover.Button
+                      className={classNames(
+                        open ? "text-gray-900" : "text-gray-500",
+                        "group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                      )}
+                    >
+                      <span>My Projects</span>
+                      <ChevronDownIcon
+                        className={classNames(
+                          open ? "text-gray-600" : "text-gray-400",
+                          "ml-2 h-5 w-5 group-hover:text-gray-500"
+                        )}
+                        aria-hidden="true"
+                      />
+                    </Popover.Button>
+
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-1"
+                    >
+                      <Popover.Panel className="absolute -ml-4 mt-3 transform z-10 px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                          <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                            {projects.map((item) => (
+                              <a
+                                key={item.name}
+                                href={item.href}
+                                className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                              >
+                                <item.icon
+                                  className="flex-shrink-0 h-6 w-6 text-red-600"
+                                  aria-hidden="true"
+                                />
+                                <div className="ml-4">
+                                  <p className="text-base font-medium text-gray-900">
+                                    {item.name}
+                                  </p>
+                                  <p className="mt-1 text-sm text-gray-500">
+                                    {item.description}
+                                  </p>
+                                </div>
+                              </a>
+                            ))}
+                          </div>
+                          <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
+                            {callsToAction.map((item) => (
+                              <div key={item.name} className="flow-root">
+                                <a
+                                  href={item.href}
+                                  className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100"
+                                >
+                                  <item.icon
+                                    className="flex-shrink-0 h-6 w-6 text-gray-400"
+                                    aria-hidden="true"
+                                  />
+                                  <span className="ml-3">{item.name}</span>
+                                </a>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      </Popover.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Popover>
+
+              <a
+                href="#"
+                className="text-base font-medium text-gray-500 hover:text-gray-900"
+              >
+                Current Project
+              </a>
+              <a
+                href="#"
+                className="text-base font-medium text-gray-500 hover:text-gray-900"
+              >
+                Contact Me
+              </a>
+            </Popover.Group>
+            {/* <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+              <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                Sign in
+              </a>
+              <a
+                href="#"
+                className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700"
+              >
+                Sign up
+              </a>
+            </div> */}
+          </div>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="duration-200 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel
+            focus
+            className="absolute top-0 inset-x-0 z-10 p-2 transition transform origin-top-right md:hidden"
+          >
+            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+              <div className="pt-5 pb-6 px-5">
+                <div className="flex items-center justify-between">
+                  <div>
+                   <h1> Natalie Fairbourne Portfolio</h1>
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
+                      <span className="sr-only">Close menu</span>
+                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <nav className="grid gap-y-8">
+                    {projects.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
+                      >
+                        <item.icon
+                          className="flex-shrink-0 h-6 w-6 text-red-600"
+                          aria-hidden="true"
+                        />
+                        <span className="ml-3 text-base font-medium text-gray-900">
+                          {item.name}
+                        </span>
+                      </a>
+                    ))}
+                  </nav>
+                </div>
+              </div>
+              <div className="py-6 px-5 space-y-6">
+                <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+                  <a
+                    href="#"
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
+                  >
+                    Current Project
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
+                  >
+                    Contact Me
+                  </a>
+                  {/* {resources.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="text-base font-medium text-gray-900 hover:text-gray-700"
+                    >
+                      {item.name}
+                    </a>
+                  ))} */}
+                </div>
+                {/* <div>
+                  <a
+                    href="#"
+                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700"
+                  >
+                    Sign up
+                  </a>
+                  <p className="mt-6 text-center text-base font-medium text-gray-500">
+                    Existing customer?
+                    <a href="#" className="text-red-600 hover:text-red-500">
+                      Sign in
+                    </a>
+                  </p>
+                </div> */}
+              </div>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+
+      <main className="lg:relative">
+        <div className="mx-auto max-w-7xl w-full pt-8 pb-20 text-center lg:py-48 lg:text-left">
+          <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+          <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
+          <img
+            className="absolute inset-0 w-full h-full object-cover object-left"
+            src="/images/IMG_1109.png"
+            alt=""
+          />
+        </div>
+            <h1 className="text-2xl mt-2 rtracking-tight font-bold text-gray-700  md:text-5xl">
+            Let me introduce myself . . .
+              {/* <span className="block xl:inline">Data to enrich your</span>{" "}
+              <span className="block text-red-600 xl:inline">
+                online business
+              </span> */}
+            </h1>
+            <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+            My name is Natalie Fairbourne and I live in Orem, Utah. I am a software developer
+            in training at the University of Utah Full Stack Bootcamp. I am the
+            co-founder and CPO of Yoodlize, a peer-to-peer marketplace where you can rent anything.
+             I oversee the platform and I designed the majority of the
+            app. My motivation to learn to code is to be more hands on in continued
+            development of Yoodlize.
+            <br></br>
+            I also have a food services company called Bee's Kitchen. I have three almost grown children, two 100 lb. dogs, and a husband who is also my co-founder. I am about 500 hrs into the 10,000 hours it takes to become a master software engineer!
+            </p>
+            <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
+              <div className="rounded-md shadow">
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10"
+                >
+                  See Projects
+                </a>
+              </div>
+              <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-red-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+                >
+                  Contact Me
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+     
+      </main>
+    </div>
+  )
+}
